a19c7dbd0fb6ef3d6c22a99050fbc0da79f15d24
Tsotne Meladze <tsotne.meladze@ilogos.biz>
2022-09-05 10:00:20 +0400

    Some minor changes

---
+++ Assets/Scripts/GameStartup.cs
@@ -0,0 +1,34 @@
+﻿using Leopotam.EcsLite;
+using UnityEngine;
+
+public class GameStartup : MonoBehaviour
+{
+    EcsWorld _world;
+    IEcsSystems _systems;
+
+    void Start()
+    {
+        _world = new EcsWorld();
+        _systems = new EcsSystems(_world);
+        _systems.Init();
+    }
+
+    void Update()
+    {
+        _systems?.Run();
+    }
+
+    void OnDestroy()
+    {
+        if (_systems != null)
+        {
+            _systems.Destroy();
+            _systems = null;
+        }
+        if (_world != null)
+        {
+            _world.Destroy();
+            _world = null;
+        }
+    }
+}
